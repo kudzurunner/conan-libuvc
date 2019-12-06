dac4272ec9f4618f93f9c97a4ae5ea98993a36a3
kudzurunner <sdio@tuta.io>
2019-12-07 00:38:16 +0300

    added build_policy='missing'

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="kudzurunner")
+    builder = ConanMultiPackager(username="kudzurunner", build_policy="missing")
     builder.add_common_builds()
     builder.run()
